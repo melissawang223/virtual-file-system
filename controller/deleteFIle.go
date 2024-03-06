50eba2c0319506dedceeaf5d90b064e71baed9af
Melissa Wang <melissawang22@gmail.com>
2024-03-06 20:09:34 +0800

    add delete file unit tests

--- controller/deleteFIle.go
+++ controller/deleteFIle.go
@@ -6,54 +6,52 @@ import (
 	"virtualFileSystem/model"
 )
 
-func DeleteFileController(args []string) {
+func DeleteFileController(args []string) error {
 	userName := ""
 	folderName := ""
 	fileName := ""
 
-	if len(args) >= 3 && args[0] != "" && args[1] != "" && args[2] != "" {
+	if len(args) >= 3 {
 		userName = args[0]
 		folderName = args[1]
 		fileName = args[2]
 	} else {
-		fmt.Println("Error: The Input is insufficient.")
-		return
+		return fmt.Errorf("Usage: `delete-file [username] [foldername] [filename]`")
 	}
 
 	// check userName is valid or not
 	if err := helper.CheckUserName(userName); err != nil {
-		return
+		return err
 	}
 
 	// check if this user exist
 	if !model.UserExist(userName) {
-		fmt.Printf("Error: The User %s doesn't exist.\n", userName)
-		return
+		return fmt.Errorf("Error: The %s has already existed.\n", userName)
 	}
 
 	// check folderName is valid or not
 	if err := helper.CheckFolderName(folderName); err != nil {
-		return
+		return err
 	}
 
 	// check if this user exist
 	if !model.FolderExist(userName, folderName) {
-		fmt.Printf("Error: The Folder %s doesn't exist.\n", folderName)
-		return
+		return fmt.Errorf("Error: The Folder %s doesn't exist.\n", folderName)
 	}
 
 	// check fileName is valid or not
 	if err := helper.CheckFileName(fileName); err != nil {
-		return
+		return err
 	}
 
 	// check if this user exist
 	if !model.FileExist(userName, folderName, fileName) {
-		fmt.Printf("Error: The File %s doesn't exist.\n", fileName)
-		return
+		return fmt.Errorf("Error: The File %s doesn't exist.\n", fileName)
 	}
 
 	model.DeleteFile(userName, folderName, folderName)
 
 	fmt.Printf("Delete %s in %s / %s successfully.\n", fileName, userName, folderName)
+
+	return nil
 }
---
+++ controller/deleteFIle_test.go
@@ -0,0 +1,61 @@
+package controller
+
+import "testing"
+
+func TestDeleteFileController(t *testing.T) {
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Happy Path: delete file successfully.",
+			args:    args{args: []string{"melissa", "foldername", "filename"}},
+			wantErr: false,
+		},
+		{
+			name:    "unHappy Path: delete file failed: username contains special char",
+			args:    args{args: []string{"melissa!", "foldername", "filename"}},
+			wantErr: true,
+		},
+		{
+			name:    "unHappy Path: delete file failed: foldername contains special char",
+			args:    args{args: []string{"melissa", "foldername!", "filename"}},
+			wantErr: true,
+		},
+		{
+			name:    "unHappy Path: delete file failed: filename contains special char",
+			args:    args{args: []string{"melissa", "foldername", "filename!"}},
+			wantErr: true,
+		},
+		{
+			name:    "unHappy Path: username does not exist",
+			args:    args{args: []string{"melissa2", "foldername", "filename"}},
+			wantErr: true,
+		},
+		{
+			name:    "unHappy Path: foldername does not exist",
+			args:    args{args: []string{"melissa", "foldername1", "filename"}},
+			wantErr: true,
+		},
+		{
+			name:    "unHappy Path: filename does not exist",
+			args:    args{args: []string{"melissa", "foldername", "filename1"}},
+			wantErr: true,
+		},
+	}
+
+	CreateUserController([]string{"melissa"})
+	CreateFolderController([]string{"melissa", "foldername"})
+	CreateFileController([]string{"melissa", "foldername", "filename"})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := DeleteFileController(tt.args.args); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteFileController() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
