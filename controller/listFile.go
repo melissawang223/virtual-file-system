1a4f58c33a504b2fcd04e773e87332097cd6d2a6
Melissa Wang <melissawang22@gmail.com>
2024-03-06 19:48:24 +0800

    refactor list file function

--- controller/listFile.go
+++ controller/listFile.go
@@ -20,25 +20,22 @@ func ListFileController(args []string) error {
 		sortType = args[2]
 		sortDir = args[3]
 	} else {
-		fmt.Println("Error: The Input is insufficient.")
-		return
+		return fmt.Errorf("Usage: `list-files [username] [foldername] [--sort-name|--sort-created] [asc|desc]`")
 	}
 
 	// check userName is valid or not
 	if err := helper.CheckUserName(userName); err != nil {
-		fmt.Println("Error: The User Input is not ok.")
-		return
+		return err
 	}
 
 	// check if this user exist
 	if !model.UserExist(userName) {
-		fmt.Printf("Error: The %s does not exist.\n", userName)
-		return
+		return fmt.Errorf("Error: The User %s doesn't exist.\n", userName)
 	}
 
 	// check folderName is valid or not
 	if err := helper.CheckFolderName(folderName); err != nil {
-		return
+		return err
 	}
 
 	// check if this user's folder exist
@@ -49,10 +46,9 @@ func ListFileController(args []string) error {
 	// check folder exist
 	currentUser := model.UsersMap[userName]
 	if len(currentUser.Folders) == 0 {
-		fmt.Printf("Warning: The %s doesn't have any folders.\n", userName)
-		return
+		return fmt.Errorf("Error: The folder %s doesn't exist.\n", folderName)
 	}
 
 	model.ListFolder(userName, sortType, sortDir)
-
+	return nil
 }
